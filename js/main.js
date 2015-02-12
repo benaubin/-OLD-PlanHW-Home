98af04005d4829ce5420f25f81e0ef8f7ea62cda
Ben! <ben@bensites.com>
2015-02-12 15:16:34 -0600

    Spaceeeeeeeeeeee

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-$( document ).ready(function() {
+$(function() {
 
     $(".sign-up").addClass("sign");
     $(".sign-in").addClass("sign");
-
+    
+    $(".sign").attr("href","");
     $( ".sign" ).click(function( event ) {
-
         alert( "Sorry, most functionality is not available yet. Please check back before next school year." );
-        $( this ).hide( "slow" );
+        $(this).hide( "slow" );
         event.preventDefault();
-
     });
+
 
 });
