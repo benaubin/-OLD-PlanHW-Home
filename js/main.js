cc3e52dd0b0c5008cc0792e63f2be67a6056cb21
Ben! <ben@bensites.com>
2015-02-12 14:52:58 -0600

    Sorry, not ready yet.

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+
+    $( ".sign" ).click(function( event ) {
+
+        alert( "Sorry, most functionality is not available yet. Please check back before next school year." );
+
+    });
+
+});
