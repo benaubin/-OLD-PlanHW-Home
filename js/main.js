529803e05bd149f5ba6d3084c167fc25d7bf198e
Ben! <ben@bensites.com>
2015-02-12 15:20:56 -0600

    Disable Function, works

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
 $(function() {
-
     $(".sign-up").addClass("sign");
     $(".sign-in").addClass("sign");
-    
-    $(".sign").attr("href","");
-    $( ".sign" ).click(function( event ) {
+    disable();
+});
+
+
+
+
+function disable(){
+    var sign = $(".sign");
+
+    sign.attr("href","");
+    sign.click(function( event ) {
         alert( "Sorry, most functionality is not available yet. Please check back before next school year." );
         $(this).hide( "slow" );
         event.preventDefault();
     });
-
-
-});
+}
